767e09869051d7305fcb49b11294319369831625
nguyenThiTham98 <nttham98@gmail.com>
2020-08-05 08:34:41 +0700

    bt

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'users/show'
   get 'static_pages/home'
   get 'static_pages/help'
-  root 'application#hello'
+  get 'static_pages/about'
+  root 'static_pages#home'
 end
